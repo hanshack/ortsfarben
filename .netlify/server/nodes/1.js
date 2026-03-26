44841764e76e248ec64ea6bbf47fd716e7a8cd55
hhack <hans.f.hack@googlemail.com>
2026-03-26 12:47:22 +0100

    change basepath

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a3add30d.js","_app/immutable/chunks/index.10d72061.js","_app/immutable/chunks/singletons.0707bff0.js","_app/immutable/chunks/index.91ade801.js"];
-export const stylesheets = [];
-export const fonts = [];
--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,9 @@ const config = {
       // instead of creating a single one for the entire app.
       // if `edge` is true, this option cannot be used
       // split: false
+      paths: {
+        base: "/ortsfarben",
+      },
     }),
   },
 
